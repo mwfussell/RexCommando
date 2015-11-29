91b2d43cc7df0d4fca76e2c3ae475184f1ab26a6
Mark <markfussell@hotmail.com>
2015-11-28 22:50:06 -0800

    Updated to Monogame v3.4 release

--- MegaMan/Properties/AssemblyInfo.cs
+++ MegaMan/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MegaMan")]
-[assembly: AssemblyProduct("MegaMan")]
+[assembly: AssemblyTitle("Rex Commando")]
+[assembly: AssemblyProduct("Rex Commando")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
